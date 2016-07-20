363e0a43cfa6625b091c279d10115e97f7283d66
Aggelos Kolaitis <neoaggelos@gmail.com>
2016-07-20 22:59:07 +0300

    Header files now defines _vsdirent_h

--- vsdirent.h
+++ vsdirent.h
@@ -1,24 +1,33 @@
+#ifndef _vsdirent_h
+#define _vsdirent_h
+
 #include <Windows.h>
 
-/* For our implementation we only need ent->d_name */
-struct dirent {
-	const char *d_name;
-};
+extern "C" {
 
-/* The DIR struct */
-typedef struct {
-	/* WinAPI specifics */
-	HANDLE h;
-	WIN32_FIND_DATAA data;
+    /* We only implement d_name */
+    struct dirent {
+        const char *d_name;
+    };
 
-	/* We are responsible for allocating-freeing the dirent memory */
-	struct dirent *ent;
+    /* The DIR struct */
+    typedef struct {
+        /* WinAPI specifics */
+        HANDLE h;
+        WIN32_FIND_DATAA data;
 
-	/* This is to not lose file data from FindFirstFile() */
-	bool readFirstFile;
-} DIR;
+        /* We are responsible for allocating-freeing the dirent memory */
+        struct dirent *ent;
 
-/* The implemented functions */
-DIR *opendir(const char* path);
-struct dirent* readdir(DIR *dir);
-void closedir(DIR* dir);
+        /* This is to not lose file data from FindFirstFile() */
+        bool readFirstFile;
+    } DIR;
+
+    /* The implemented functions */
+    DIR *opendir(const char* path);
+    struct dirent* readdir(DIR *dir);
+    void closedir(DIR* dir);
+
+}
+
+#endif /* _vsdirent_h */
